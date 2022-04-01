2885faa338501f4889b7737f1b826520dfe819a4
post-witch <justlyagushka@gmail.com>
2022-04-01 12:48:59 +0300

    starting work with bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
-const {src, dest, watch} = require('gulp');
-const {series, parallel} = require('gulp')
+const {src, dest, watch, series, parallel} = require('gulp');
 const sass = require("gulp-sass")(require('sass'));
+const cleanCss = require("gulp-clean-css");
+const rename = require("gulp-rename")
 
-
-function sassToCss() {
+function scssToCss() {
     return src('src/sass/*.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(dest('dist/css/styles.css'));
+      .pipe(cleanCss({compatibility: 'ie8'}))
+      .pipe(rename('custom.min.css'))
+      .pipe(dest('dist/css'));
   }
 
 function html() {
     return src("src/index.html")
-      .pipe(dest('dist/'))
+      .pipe(dest('dist/'));
 }
   
 
+
+
   exports.default = function() {
-    watch('src/sass/*.scss', sassToCss)
-    watch('src/index.html', html)
+    watch('src/sass/*.scss', scssToCss)
+    watch('src/index.html', html);
   }
