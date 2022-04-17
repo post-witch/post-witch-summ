eb771a5b02a0374874d3728bf289e0cc14b46e91
post-witch <justlyagushka@gmail.com>
2022-04-17 23:45:37 +0300

    close close to final

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
-
 const {src, dest, watch} = require('gulp');
 const sass = require("gulp-sass")(require('sass'));
 const cleanCss = require("gulp-clean-css");
-const rename = require("gulp-rename");
 const htmlmin = require("gulp-htmlmin");
+const concat = require("gulp-concat")
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const browserSync = require('browser-sync').create();
 
 function scss_to_css() {
-    return src('src/sass/custom.scss')
-      .pipe(sass().on('error', sass.logError))
+    return src('src/sass/*/*.scss')
+      .pipe(sass({
+        includePaths: ['./node_modules'],
+      }).on('error', sass.logError))
+      .pipe(concat('custom.min.css'))
       .pipe(cleanCss({compatibility: 'ie8'}))
-      .pipe(rename('custom.min.css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
       .pipe(dest('dist/css'));
   }
 
@@ -44,7 +48,7 @@ exports.dev_mode = function() {
         baseDir: 'dist'
       },
     })
-    watch('src/sass/*.scss', scss_to_css);
+    watch('src/sass/*/*.scss', scss_to_css);
     watch('src/index.html', html);
     watch('src/img/*.*', images);
     watch('src/fonts/*.*', fonts);
